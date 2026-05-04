ef35fe14bb91c9c1cff3f024cba90cd658f5a50c
Viet <trandaiviet78@gmail.com>
2026-05-04 20:42:24 +0700

    fix merge confict

--- user-management-service/src/main/resources/db/migration/V3__learning_path_test_data.sql
+++ user-management-service/src/main/resources/db/migration/V3__learning_path_test_data.sql
@@ -12,7 +12,7 @@ INSERT INTO user_management.users (id, email, avatar_url, first_name, last_name,
 VALUES ('9b2c7d39-bc76-41ec-8586-d64cbc18775b', 'test.002@hcmut.edu.vn', NULL, 'FIRSTYEAR', 'STUDENT', '0900000002', NULL, '1ba8a25e-70cd-430d-8dbe-92d12f178613', 'e5909d1e-492d-4419-9766-ce69821e28ec', now(), now())
 ON CONFLICT (email) DO NOTHING;
 INSERT INTO user_management.students (user_id, student_code, intake_year_id, department_id)
-VALUES ('9b2c7d39-bc76-41ec-8586-d64cbc18775b', '2110002', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
+VALUES ('9b2c7d39-bc76-41ec-8586-d64cbc18775b', '2210002', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
 ON CONFLICT (student_code) DO NOTHING;
 
 --test010
@@ -28,7 +28,7 @@ INSERT INTO user_management.users (id, email, avatar_url, first_name, last_name,
 VALUES ('37f5a1d9-83ab-45c4-85de-0205d1ca1a0b', 'test.003@hcmut.edu.vn', NULL, 'SECONDYEAR', 'STUDENT', '0900000003', NULL, '1ba8a25e-70cd-430d-8dbe-92d12f178613', 'e5909d1e-492d-4419-9766-ce69821e28ec', now(), now())
 ON CONFLICT (email) DO NOTHING;
 INSERT INTO user_management.students (user_id, student_code, intake_year_id, department_id)
-VALUES ('37f5a1d9-83ab-45c4-85de-0205d1ca1a0b', '2110003', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
+VALUES ('37f5a1d9-83ab-45c4-85de-0205d1ca1a0b', '2210003', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
 ON CONFLICT (student_code) DO NOTHING;
 
 --test004
@@ -36,7 +36,7 @@ INSERT INTO user_management.users (id, email, avatar_url, first_name, last_name,
 VALUES ('94bc2015-b87f-45ac-8b95-a7b45f2faa47', 'test.004@hcmut.edu.vn', NULL, 'HIGH', 'ACHIEVER', '0900000004', NULL, '1ba8a25e-70cd-430d-8dbe-92d12f178613', 'e5909d1e-492d-4419-9766-ce69821e28ec', now(), now())
 ON CONFLICT (email) DO NOTHING;
 INSERT INTO user_management.students (user_id, student_code, intake_year_id, department_id)
-VALUES ('94bc2015-b87f-45ac-8b95-a7b45f2faa47', '2110004', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
+VALUES ('94bc2015-b87f-45ac-8b95-a7b45f2faa47', '2210004', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
 ON CONFLICT (student_code) DO NOTHING;
 
 --test005
@@ -44,7 +44,7 @@ INSERT INTO user_management.users (id, email, avatar_url, first_name, last_name,
 VALUES ('4d3f8e19-d6f8-475f-b534-ffec14382540', 'test.005@hcmut.edu.vn', NULL, 'STRUGGLING', 'STUDENT', '0900000005', NULL, '1ba8a25e-70cd-430d-8dbe-92d12f178613', 'e5909d1e-492d-4419-9766-ce69821e28ec', now(), now())
 ON CONFLICT (email) DO NOTHING;
 INSERT INTO user_management.students (user_id, student_code, intake_year_id, department_id)
-VALUES ('4d3f8e19-d6f8-475f-b534-ffec14382540', '2110005', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
+VALUES ('4d3f8e19-d6f8-475f-b534-ffec14382540', '2210005', '9ab1203e-27f6-4aa9-8f91-3a1980b03ba8', 'f6f1f3bd-cb99-4ec2-8a45-16d59c2c1371')
 ON CONFLICT (student_code) DO NOTHING;
 
 --test008
