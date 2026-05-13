95990785e1aad48854f3306a29432261d063ef50
dphuongdev <duyphuong.devg@gmail.com>
2026-05-13 17:09:16 +0700

    fix something

--- api-gateway/src/main/java/com/hcmut/lms/gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/hcmut/lms/gateway/config/CorsConfig.java
@@ -23,7 +23,8 @@ public class CorsConfig {
         corsConfig.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",  // React dev server
                 "http://localhost:5173",  // Vite dev server
-                "http://localhost:4200" // Angular dev server,
+                "http://localhost:4200", // Angular dev server,
+                "https://lms.dphuongdev.io.vn"
 
         ));
         
